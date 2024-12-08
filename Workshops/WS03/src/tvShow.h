8edcc168bb53840f1ac3f9ea1ce7185156d7ad64
Poxxon <>
2024-12-08 17:59:06 -0500

    Workshop 4 released

--- Workshops/WS03/src/tvShow.h
+++ Workshops/WS03/src/tvShow.h
@@ -1,3 +1,15 @@
+/*===============================================================
+Name:           Pouya Rad
+Student Number: 164382228
+Email:          prad@myseneca.ca
+Section:        NDD
+
+I declare that this submission is the result of my own work and I
+only copied the code that my professor provided to complete my
+workshops and assignments. This submitted piece of work has not
+been shared with any other student or 3rd party content provider.
+=================================================================*/
+
 #ifndef SENECA_TVSHOW_H
 #define SENECA_TVSHOW_H
 
